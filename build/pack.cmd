fe4dc156721199fe50c70534a234438a9657e959
Adam Craven <adammcraven@gmail.com>
2018-06-24 14:23:18 +1000

    Added deprecation notice to nuspec

--- build/pack.cmd
+++ build/pack.cmd
@@ -1,4 +1,6 @@
-SET msbuild="C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe"
+SET msbuild="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe"
+
+..\tools\nuget\nuget.exe restore ..\src\ChannelAdam.Core.sln
 
 %msbuild% ..\src\ChannelAdam.Core\ChannelAdam.Core.csproj /t:Rebuild /p:Configuration=Release;TargetFrameworkVersion=v4.0;OutDir=bin\Release\net40
 %msbuild% ..\src\ChannelAdam.Core\ChannelAdam.Core.csproj /t:Rebuild /p:Configuration=Release;TargetFrameworkVersion=v4.5;OutDir=bin\Release\net45
--- build/push.cmd
+++ build/push.cmd
@@ -4,6 +4,8 @@ set packageName=ChannelAdam.Core
 
 set /p version="What is the version you want to push?"
 
-..\tools\nuget\nuget.exe push "%packageName%.%version%.nupkg"
+..\tools\nuget\nuget.exe push "%packageName%.%version%.nupkg" -Source https://www.nuget.org
+
+..\tools\nuget\nuget.exe push "%packageName%.%version%.Symbols.nupkg" -Source https://nuget.smbsrc.net/
 
 pause
